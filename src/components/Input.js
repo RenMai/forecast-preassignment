9c2f6e88647a9e7d1bbb2d031de28e114e66d75b
Nhan Mai <maithieunhan@gmail.com>
2022-10-10 02:08:56 +0300

    getting done

---
+++ src/components/Input.js
@@ -0,0 +1,70 @@
+import { useEffect, useRef, useState } from "react";
+import { getCoordintes } from "../helper/utility";
+let message = "";
+
+const Input = ({
+  onInputHandlerChange,
+  updateWeatherHandler,
+  query,
+  changeHandler,
+  btnDisable,
+}) => {
+  const inputRef = useRef(null);
+  const [currentCity, setCurrentCity] = useState("");
+
+  useEffect(() => {
+    if (currentCity !== "") {
+      inputRef.current.value = currentCity;
+    }
+  }, [currentCity]);
+
+  const handleSelectCurrentLocation = async () => {
+    getCoordintes(changeHandler, setCurrentCity);
+  };
+  return (
+    <div>
+      <h2 className="w3-text-blue">Weather Forecast</h2>
+      <div>
+        <div>
+          <label className="w3-text-blue">
+            <b>City</b>
+          </label>
+          <form style={{ display: "flex" }}>
+            <input
+              ref={inputRef}
+              className="w3-input w3-border"
+              placeholder="Enter the name of city"
+              onChange={(event) => {
+                onInputHandlerChange(event);
+              }}
+              
+              type="text"
+            />
+            <button
+              style={{
+                backgroundColor: "#55ba52",
+                border: "none",
+                width: 50,
+                color: "white",
+              }}
+              onClick={() => handleSelectCurrentLocation()}
+            >
+              <i className="fa fa-map-marker" aria-hidden="true"></i>
+            </button>
+          </form>
+        </div>
+        <p>
+          <button id="submitBtn"
+            className="w3-btn w3-blue"
+            onClick={() => updateWeatherHandler()}
+            disabled={query.length < 1 || btnDisable}
+          >
+            Show weather info
+          </button>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Input;
